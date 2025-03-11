1cdecd09b4d24bd1458b776294bbf938c30cbdb6
matteo-rossi90 <matteo.rossi1314@gmail.com>
2025-03-11 18:01:01 +0100

    feat: create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+
 import './App.css'
+
+import Header from './components/Header/Header'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      Ciao
+      <Header/>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
     </>
   )
 }
